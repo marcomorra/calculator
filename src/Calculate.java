d6fdf4024b55212eacfe0f93237de4a6791c38ca
Alexey <foxitq@gmail.com>
2022-09-14 14:52:37 +0300

    Add new changes

--- src/Calculate.java
+++ src/Calculate.java
@@ -1,26 +1,61 @@
-public class Operation {
+import java.util.Scanner;
+public class Calculate {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-
-        System.out.println(dev(10, 2));
+        double num1 = getNumberFromConsole();
+        double num2 = getNumberFromConsole();
+        char operation = symbol();
+        double result = calc(num1, num2, operation);
+        System.out.println("Ваш результат: " + result);
     }
-
-    public static double sum(double arg1, double arg2) {
-        double sum = arg1 + arg2;
-        return sum;
+    public static double getNumberFromConsole() {
+        System.out.println("Введите число:");
+        double number;
+        if (sc.hasNextDouble()) {
+            number = sc.nextDouble();
+        } else {
+            System.out.println("Вы не ввели число, повторите программу заного");
+            sc.nextInt();
+            number = getNumberFromConsole();
+        }
+        return number;
     }
+        public static char symbol(){
+            System.out.println("Введите операцию: ");
+            char operation;
+            if (sc.hasNext()){
+                operation = sc.next().charAt(0);
+            } else {
+                System.out.println("Вы ввели неподдерживаему операцию");
+//                sc.next();
+                operation = symbol();
+            }
+            return operation;
+        }
 
-    public static double sub(double arg1, double arg2) {
-        double sub = arg1 - arg2;
-        return sub;
-    }
-
-    public static double mult(double arg1, double arg2) {
-        double mult = arg1 * arg2;
-        return mult;
-    }
-
-    public static double dev(double arg1, double arg2) {
-        double dev = arg1 / arg2;
-        return dev;
+    public static double calc(double arg1, double arg2, char operation){
+        double result;
+        switch (operation) {
+            case '+':
+                result = arg1 + arg2;
+                break;
+            case '-':
+                result = arg1 - arg2;
+                break;
+            case '*':
+                result = arg1 * arg2;
+                break;
+            case '/':
+                if (arg2 == 0) {
+                    System.out.println("На ноль делить нельзя!");
+                } else {
+                    result = arg1 / arg2;
+                    break;
+                }
+            default:
+                System.out.println("Калькулятор не поддерживает данную операцию");
+                result = calc(arg1, arg2, operation);
+        }
+        return result;
     }
 }
